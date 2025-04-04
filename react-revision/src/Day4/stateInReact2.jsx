3815458fe98a470a6a9671d456b731431eb5864b
Ali Arslan <arsalanli873@gmail.com>
2025-04-04 22:48:23 +0500

    Minor chnages

--- react-revision/src/Day4/stateInReact2.jsx
+++ react-revision/src/Day4/stateInReact2.jsx
@@ -9,16 +9,21 @@ const PersonInfo2=()=>{
     const [name,setName]=useState("______");
     const [age,setAge]=useState(0);
     
-    const randomIndex=Math.floor(Math.random()*names.length);
+    const randomIndex=Math.floor(Math.random()*names.length);   
 
     const whenClicked=()=>{
         setName(names[randomIndex]);
         setAge(a=>a+3);
     }
+    const whenClicked2=()=>{
+        setAge(a=>a+1);
+    }
 
     return(
         <div>
-            <p>Click button to see magic <button style={{marginTop:"20px"}} onClick={whenClicked}>Click me</button></p>
+            <p>Click this to increment age by 3 <button style={{marginTop:"20px",border:"2px sol"}} onClick={whenClicked}>Click me</button>+3</p>
+            
+            <p>Click this to increment age by 1 <button onClick={whenClicked2}>Click me</button>+1</p>
             <p>The student's name is <b>{name}</b></p>
             <p>The person's age is <em> {age} </em></p>
         </div>
